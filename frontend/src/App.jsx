4c778c71da2a61fcc8f0da12392eb47d1fd91150
Jeromel-Pushparaj <jeromal03@gmail.com>
2025-06-13 20:05:42 +0530

    feat: added react router and /login rout with login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { Button } from '@/components/ui/button.jsx'
-
+import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import DashboardPage from './app/dashboard/page';
+import LoginPage from './app/login/page';
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className=' flex justify-center items-center flex-col'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <Button>shadcn button</Button>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Routes>
+      <Route path='/' element={ <DashboardPage/>}/>
+      <Route path='/login' element={ <LoginPage /> }/>
+    </Routes>
     </>
   )
 }
