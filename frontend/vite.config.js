0b749c70d488c67d56cb2f62bff85acd7d5eb45b
Jeromel-Pushparaj <jeromal03@gmail.com>
2025-06-13 13:38:46 +0530

    conf: vite server configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // This makes it listen on all interfaces
+    port: 5173,
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
