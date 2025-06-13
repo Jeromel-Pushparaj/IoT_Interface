7f22dcc964bc4d4e7e295eaeff44946b105add58
Jeromel-Pushparaj <jeromal03@gmail.com>
2025-06-13 12:39:52 +0530

    feat: Installed Tailwindcss and shadcn for styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from '@/components/ui/button.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <div className=' flex justify-center items-center flex-col'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -17,6 +18,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Button>shadcn button</Button>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
