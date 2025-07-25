57791879362cd8041945a07abd15d97d7c107855
Gautam Suthar <iamgautamsuthar@outlook.com>
2025-07-25 14:56:56 +0530

    Refactor import paths in App.jsx and main.jsx to use aliasing; update jsconfig.json for path resolution

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,40 @@
-import './App.css'
-import React, { useEffect } from 'react'
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import { isTokenExpired, logoutAndRedirect } from './utils/auth'; // Adjust the import path as necessary
-import DashboardPage from './app/dashboard/page';
-import LoginPage from './app/login/page';
-import SingupPage from './app/signup/page';
-import DashboardTest from './app/home/dashboard';
-import DevicePage from './app/device/page';
-import AddDevice from './app/device/AddDevice';
-import ControlPage from './app/control/page'; // Import the ControlPage component
+import "./App.css";
+import React, { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { isTokenExpired, logoutAndRedirect } from "@utils/auth";
+import DashboardPage from "@app/dashboard/page";
+import LoginPage from "@app/login/page";
+import SingupPage from "@app/signup/page";
+import DashboardTest from "@app/home/dashboard";
+import DevicePage from "@app/device/page";
+import AddDevice from "@app/device/AddDevice";
+import ControlPage from "@app/control/page";
 
 function App() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    if (isTokenExpired()) {
-      logoutAndRedirect(navigate);
-    }
-  }, []);
+    useEffect(() => {
+        if (isTokenExpired()) {
+            logoutAndRedirect(navigate);
+        }
+    }, []);
 
-  return (
-    <>
-    <Routes>
-      <Route path='/' element={ <DashboardPage/>}/>
-      
-      // Routes for Devices
-      <Route path='/device' element={ <DevicePage/>}/> 
-      <Route path='/device/add' element={ <AddDevice/> }/>
-
-      //Routes for Controll page
-      <Route path='/control' element={ <ControlPage/> }/>
-      <Route path='/control/:deviceId' element={ <ControlPage/> }/>
-
-      <Route path='/login' element={ <LoginPage /> }/>
-      <Route path='/signup' element={ <SingupPage /> }/>
-      <Route path='/test' element={ <DashboardTest /> }/>
-    </Routes>
-    </>
-  )
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<DashboardPage />} />
+                {/* // Routes for Devices */}
+                <Route path="/device" element={<DevicePage />} />
+                <Route path="/device/add" element={<AddDevice />} />
+                {/* //Routes for Controll page */}
+                <Route path="/control" element={<ControlPage />} />
+                <Route path="/control/:deviceId" element={<ControlPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SingupPage />} />
+                <Route path="/test" element={<DashboardTest />} />
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
